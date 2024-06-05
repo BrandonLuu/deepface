be4921acd7ce2d3b837ea5dc637ff11063daddef
Brandon Luu <brandonluu1@gmail.com>
2024-06-05 00:59:28 -0700

    Added random right/left eye output

--- tests/eyes.py
+++ tests/eyes.py
@@ -7,6 +7,7 @@ import pickle
 import os
 import cv2
 import numpy as np
+import random
 
 # project dependencies
 from deepface import DeepFace
@@ -90,9 +91,9 @@ def convert_to_openCV_format(face):
 if __name__ == "__main__":
     
     # Open the input movie file
-    input_file = "girl_face_1920_1080_25fps.mp4"
-    
-    input_movie = cv2.VideoCapture("face_only/"+input_file)
+    face_dir = os.path.join(os.getcwd(), 'face_only')
+    input_file = os.path.join(face_dir, "girl_face_1920_1080_25fps.mp4")
+    input_movie = cv2.VideoCapture(input_file)
     length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
     
     # Create an output movie file (make sure resolution/frame rate matches input video!)
@@ -104,11 +105,14 @@ if __name__ == "__main__":
     frame_height = eye_crop_width * 2
     framerate = int(input_movie.get(cv2.CAP_PROP_FPS))
     
-    right_file = f'face_only/{input_file}_right.avi'
-    left_file = f'face_only/{input_file}_left.avi'
-    face_file = f'face_only/{input_file}_face.avi'
+    right_file = os.path.join(face_dir, f'{input_file}_right.avi')
+    left_file = os.path.join(face_dir, f'{input_file}_left.avi')
+    rand_file = os.path.join(face_dir, f'{input_file}_rand.avi')
+    # face_file = os.path.join('face_only', f'{input_file}_face.avi')
+    
     out_movie_right = cv2.VideoWriter(right_file, fourcc, framerate, (frame_width, frame_height))
     out_movie_left = cv2.VideoWriter(left_file, fourcc, framerate, (frame_width, frame_height))
+    out_movie_rand = cv2.VideoWriter(rand_file, fourcc, framerate, (frame_width, frame_height))
     # out_movie_face = cv2.VideoWriter(face_file, fourcc, framerate, (554, 55))
     
     frame_number = 0
@@ -122,6 +126,8 @@ if __name__ == "__main__":
         except FileNotFoundError:
             logger.info("=== No pickle")
             
+            
+    eye_choice = "right"
     while(input_movie.isOpened()):
         
         # Grab a single frame of video
@@ -140,12 +146,12 @@ if __name__ == "__main__":
             # 1st Pass: Create normalized face
             face_obj = DeepFace.extract_faces(img_path=frame, detector_backend=detector_backends[0], enforce_detection=False, align=True, expand_percentage=0)
             face_obj = face_obj[0] # HARDCODE: processing only 1st detected face
-            face = convert_to_openCV_format(face_obj["face"])
+            face = convert_to_openCV_format(face_obj["face"]) # Convert to image for re-extraction
             
             # 2st Pass: process Eyes of normalized face with adjusted Eye coordinates
             face_obj = DeepFace.extract_faces(img_path=face, detector_backend=detector_backends[0], enforce_detection=False, align=True, expand_percentage=0)
-            face_obj = face_obj[0]
-            face = convert_to_openCV_format(face_obj["face"])
+            face_obj = face_obj[0] # HARDCODE: processing only 1st detected face
+            face = convert_to_openCV_format(face_obj["face"]) # Convert to image for outputting 
         
         # Get Eyes
         right_eye = face_obj["facial_area"]["right_eye"]
@@ -156,9 +162,9 @@ if __name__ == "__main__":
         
         # Crop Eyes
         right_eye_crop = face[ right_eye[0] - eye_crop_width : right_eye[0] + eye_crop_width, \
-                                right_eye[1] - eye_crop_width : right_eye[1] + eye_crop_width]
+                               right_eye[1] - eye_crop_width : right_eye[1] + eye_crop_width]
         left_eye_crop =  face[ left_eye[1] - eye_crop_width : left_eye[1] + eye_crop_width, \
-                                left_eye[0] - eye_crop_width : left_eye[0] + eye_crop_width]
+                               left_eye[0] - eye_crop_width : left_eye[0] + eye_crop_width]
         
         # Show each frame
         # if cv2.waitKey(1000) == ord('q'): break
@@ -170,6 +176,21 @@ if __name__ == "__main__":
         out_movie_right.write(right_eye_crop)
         out_movie_left.write(left_eye_crop)
         # out_movie_face.write(face)
+
+        rand_eye = right_eye_crop if eye_choice == "right" else left_eye_crop
+        switch = random.randint(1,4)
+        if switch == 1: # chance to flip the eye output
+            if eye_choice == "right":
+                print("switching to left")
+                rand_eye = left_eye_crop
+                eye_choice = "left"
+            else:
+                print("switching to right")
+                rand_eye = right_eye_crop
+                eye_choice = "right"
+            
+        out_movie_rand.write(rand_eye)
+        
     
     # Write to pickle
     if PICKLE_TESTING_ENABLE and not os.path.isfile("face_objs.pkl"): # Write out frame_face_objs
