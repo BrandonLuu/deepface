c124fab2175e9e876c73c6c78e9018e2099b7034
Brandon Luu <brandonluu1@gmail.com>
2024-06-13 00:08:41 -0700

    Added grid offset edit style

--- tests/eyes_edits.py
+++ tests/eyes_edits.py
@@ -12,8 +12,8 @@ from deepface.commons import logger as log
 logger = log.get_singletonish_logger()
 
 # Output Dimensions
-out_width = 200
-out_height = 200
+OUT_WIDTH = 200
+OUT_HEIGHT = 200
 
 
 # === OpenCV Utility Functions ===
@@ -76,24 +76,24 @@ def change_background(out_frame, in_frame):
     bg_select = random.randint(1,4)
     if bg_select == 1: # np resize full scale - tile
         # Whacky frame tile code
-        resize_frame = np.resize(in_frame, (out_height, out_width, 3))
-        resize_frame = cv2.resize(resize_frame, (out_height, out_width), interpolation = cv2.INTER_LINEAR)
+        resize_frame = np.resize(in_frame, (OUT_HEIGHT, OUT_WIDTH, 3))
+        resize_frame = cv2.resize(resize_frame, (OUT_HEIGHT, OUT_WIDTH), interpolation = cv2.INTER_LINEAR)
         overlay_image(out_frame, resize_frame, 0, 0)
         
     elif bg_select == 2: # np resize 75% - stutter tile
         resize_percentage = 0.75
-        resize_frame = np.resize(in_frame, (int(out_height * resize_percentage), int(out_width * resize_percentage), 3))
-        resize_frame = cv2.resize(resize_frame, (out_height, out_width), interpolation = cv2.INTER_LINEAR)
+        resize_frame = np.resize(in_frame, (int(OUT_HEIGHT * resize_percentage), int(OUT_WIDTH * resize_percentage), 3))
+        resize_frame = cv2.resize(resize_frame, (OUT_HEIGHT, OUT_WIDTH), interpolation = cv2.INTER_LINEAR)
         overlay_image(out_frame, resize_frame, 0, 0)
         # cv_show_wrapper(out_frame)
 
     else: # Default - scale to frame
         in_width, in_height, in_rgb = in_frame.shape
-        scale_x, scale_y = (out_width / in_width),  (out_height / in_height)
+        scale_x, scale_y = (OUT_WIDTH / in_width),  (OUT_HEIGHT / in_height)
         out_frame = cv2.resize(in_frame, None, fx = scale_x, fy = scale_y, interpolation = cv2.INTER_CUBIC)
 
 
-def get_rand_eye_choice(eye_choice, ret, total_outcomes):
+def rand_flip_eye_choice(eye_choice, ret, total_outcomes):
     # Random eye frame write - read/write from eye_choice
     if random.randint(1, total_outcomes) == 1:  # flip to other eye if success and other eye has frames
         eye_choice = "right" if eye_choice == "left" else "left"
@@ -117,8 +117,8 @@ def edit_eyes(in_right_file, in_left_file):
 
     # Configure output movie file (make sure resolution/frame rate matches input video!)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # type: ignore
-    frame_width = out_width
-    frame_height = out_height
+    frame_width = OUT_WIDTH
+    frame_height = OUT_HEIGHT
     framerate = int(in_movie_right.get(cv2.CAP_PROP_FPS)) # TODO: how to output/respeed in/out framerate
 
     # Create Output movies
@@ -133,8 +133,8 @@ def edit_eyes(in_right_file, in_left_file):
     
     # Random video overlay offset ranges init
     offset_percentage = 0.75
-    offset_x = int(-(out_width * offset_percentage))
-    offset_y = int(out_height + (out_height * offset_percentage))
+    offset_x = int(-(OUT_WIDTH * offset_percentage))
+    offset_y = int(OUT_HEIGHT + (OUT_HEIGHT * offset_percentage))
     x_offset_right, y_offset_right = random.randint(offset_x, offset_y), random.randint(offset_x, offset_y)
     x_offset_left, y_offset_left = random.randint(offset_x, offset_y), random.randint(offset_x, offset_y)    
     
@@ -155,28 +155,27 @@ def edit_eyes(in_right_file, in_left_file):
         # Quit when the input video file ends
         if ret["right"] == False and ret["left"] == False:
             break
-
-        get_rand_eye_choice(eye_choice, ret, total_outcomes=4)
         
-        # Random eye offset changes
+        
+        # Change background or fullscreen a random eye
+        rand_flip_eye_choice(eye_choice, ret, total_outcomes=4)
+        if random.randint(1,4) == 1:
+            change_background(out_frame, in_frame[eye_choice])
+        else:
+            in_width, in_height, in_rgb = in_frame[eye_choice].shape
+            scale_x, scale_y = (OUT_WIDTH / in_width),  (OUT_HEIGHT / in_height)
+            out_frame = cv2.resize(in_frame[eye_choice], None, fx=scale_x, fy=scale_y, interpolation=cv2.INTER_CUBIC)
+
+
+    # Overlay Right/Left Eyes
+        # Random eye offset changes - 25% chance frame moves
         if random.randint(1, 4) == 1:
             x_offset_right, y_offset_right = random.randint(offset_x, offset_y), random.randint(offset_x, offset_y)
             # logger.info(f"R Moving {x_offset_right}, {y_offset_right}")
         if random.randint(1, 4) == 1:
             x_offset_left, y_offset_left = random.randint(offset_x, offset_y), random.randint(offset_x, offset_y)
             # logger.info(f"L Moving {x_offset_left}, {y_offset_left}")
-        
-        
-        # Change background or fullscreen an eye
-        if random.randint(1,4) == 1:
-            change_background(out_frame, in_frame[eye_choice])
-        else:
-            in_width, in_height, in_rgb = in_frame[eye_choice].shape
-            scale_x, scale_y = (out_width / in_width),  (out_height / in_height)
-            out_frame = cv2.resize(in_frame[eye_choice], None, fx=scale_x, fy=scale_y, interpolation=cv2.INTER_CUBIC)
-
-
-        # Overlay Right/Left Eyes
+            
         if ret["right"]:
             overlay_image(out_frame, in_frame["right"], x_offset_right, y_offset_right)
         if ret["left"]:
@@ -193,6 +192,88 @@ def edit_eyes(in_right_file, in_left_file):
     out_movie.release()
 
 
+def edit_grid_eyes(in_right_file, in_left_file):
+    # Open the input movie file
+    in_movie_right = cv2.VideoCapture(in_right_file)
+    in_movie_left = cv2.VideoCapture(in_left_file)
+    length_right = int(in_movie_right.get(cv2.CAP_PROP_FRAME_COUNT))
+    length_left = int(in_movie_left.get(cv2.CAP_PROP_FRAME_COUNT))
+    logger.info(f"Grid edit input lengths R:{length_right} | L:{length_left} ")
+    
+    # Configure output movie file (make sure resolution/frame rate matches input video!)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # type: ignore
+    frame_width = 1080
+    frame_height = 1350
+    framerate = int(in_movie_right.get(cv2.CAP_PROP_FPS)) # TODO: how to output/respeed in/out framerate
+
+    out_grid_file = os.path.join(os.getcwd(), "face_only", "right-left-edit", "grid-edit.mp4")
+    out_grid_movie = cv2.VideoWriter(out_grid_file, fourcc, framerate, (frame_width, frame_height))
+    
+    # Read in all frames of R/L video (only up to the shortest input)
+    frame_number = 0
+    in_frame = {"right":[], "left": []}
+    
+    while in_movie_right.isOpened or in_movie_left.isOpened:
+        # if frame_number % 25 == 0:
+        #     logger.info(f"Processing Grid frame {frame_number} / R:{length_right} | L:{length_left} ")
+
+        # Read a single frame of video
+        ret = {}
+        ret["right"], right_frame = in_movie_right.read()
+        ret["left"], left_frame = in_movie_left.read()
+        
+        # Quit when shortest video ends
+        if ret["right"] == False or ret["left"] == False:
+            break
+        
+        # Add to frame list
+        in_frame["right"].append(right_frame)
+        in_frame["left"].append(left_frame)
+        
+        frame_number += 1
+    
+    # for i in range(5, len(in_frame["right"]), 25):
+    #     if cv_show_wrapper(in_frame["right"][i]): return
+    
+    frame_number = 0
+    rows, cols = 5, 4
+    offset_grid = generate_grid(rows, cols, 100)
+    # print_grid(offset_grid)
+    
+    in_len = {}
+    in_len["right"] = len(in_frame["right"])
+    # in_left_len = len(in_frame["left"])
+    
+    width_step = int(frame_width / cols)
+    height_step = int(frame_height/ rows)
+    logger.info(f"Grid: step size: {width_step} x {height_step}")
+    
+    # eye_choice = "right"
+    # while in_movie_right.isOpened or in_movie_left.isOpened:
+    for frame_number in range(in_len["right"]):
+        out_frame = np.zeros((frame_height, frame_width, 3), dtype=np.uint8)
+        logger.info(f"Frame: {frame_number}")
+        
+        # Write all subframes to out_frame
+        for i in range(rows):
+            for j in range(cols):
+                # TODO: add random R/L flip per cell
+                # rand_flip_eye_choice(eye_choice, ret, total_outcomes=4)
+                
+                subframe_x, subframe_y = i * width_step, j * height_step
+                subframe_offset = (frame_number + offset_grid[i][j]) % in_len["right"]
+                
+                subframe = in_frame["right"][subframe_offset]
+                resize_frame = cv2.resize(subframe, (width_step, height_step), interpolation = cv2.INTER_LINEAR)
+                
+                # logger.info(f"Subframe ({i},{j}): Dimension:({subframe_x},{subframe_y}) ")
+                overlay_image(out_frame, resize_frame, subframe_x, subframe_y)
+
+        # if cv_show_wrapper(out_frame): return
+        out_grid_movie.write(out_frame)
+    
+ 
+
 if __name__ == "__main__":
     logger.info("=== Beginning Edits ===")
     
@@ -204,9 +285,17 @@ if __name__ == "__main__":
     face_dir = os.path.join(os.getcwd(), "face_only", "right-left-edit")
     right_file = os.path.join(face_dir, right_vid)
     left_file = os.path.join(face_dir, left_vid)
+    # logger.info(f"Editing Files: \nR: {right_file} \nL: {left_file}")
+    # edit_eyes(right_file, left_file)
     
+    right_vid = "still-girl-face-right.mp4"
+    left_vid = "still-girl-face-left.mp4"
     logger.info(f"Editing Files: \nR: {right_file} \nL: {left_file}")
     
-    edit_eyes(right_file, left_file)
+    face_dir = os.path.join(os.getcwd(), "face_only", "right-left-edit")
+    right_file = os.path.join(face_dir, right_vid)
+    left_file = os.path.join(face_dir, left_vid)
+    edit_grid_eyes(right_file, left_file)
+
     
     cv2.destroyAllWindows()
